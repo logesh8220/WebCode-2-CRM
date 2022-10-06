1336164e92cc3112b4209d232667cc6584e63a90
logesh8220 <logeshthirumurugan@gmail.com>
2022-10-06 10:50:45 +0530

    added loading

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -7,7 +7,7 @@ import 'react-toastify/dist/ReactToastify.min.css';
 import { env } from '../Config'
 
 function Login() {
-    // const [datas, setdata] = useState([])
+    const [isloading, setloading] = useState(false)
     const Navigateto = useNavigate()
     const formik = useFormik({
         initialValues: {
@@ -16,6 +16,7 @@ function Login() {
         },
         onSubmit: async values => {
             try {
+                setloading(true)
                 let data = await axios.post(`${env.api}/login`, values)
                 let getdata = await axios.get(`${env.api}/auth/${values.Email}`)
                 if (!data.status === 200) {
@@ -40,7 +41,7 @@ function Login() {
                             break;
                         case "Admin":
                             Navigateto(`/Admin/${getdata.data[0].FirstName}`)
-                             toast(`${`Welcome ${getdata.data[0].FirstName}`}`, {
+                            toast(`${`Welcome ${getdata.data[0].FirstName}`}`, {
                                 className: "tost-massage"
                             })
                             break;
@@ -48,6 +49,7 @@ function Login() {
                     }
 
                 }
+                setloading(false)
             } catch (error) {
                 console.log(error)
                 toast.error(error.response.data.message, {
@@ -87,20 +89,27 @@ function Login() {
 
             </div>
             <div className='d-flex justify-content-center'>
-                <form className='col-lg-3 container auth ' onSubmit={formik.handleSubmit}>
-                    <div className='text-center mb-5'>
-                        <img src='https://wishlistmember.com/wp-content/uploads/2021/08/fluentCRM.png' style={{ width: "50px", borderRadius: "5px" }}></img>
-                        <h4>fluentCRM</h4>
-                    </div>
-                    <input name='Email' placeholder='Email' className='form-control mb-5' onChange={formik.handleChange} value={formik.values.Email}></input>
-                    <input name='Password' type="password" placeholder='Password' className='form-control mb-5' onChange={formik.handleChange} value={formik.values.Password}></input >
-                    <div class="d-grid gap-2 mt-2 text-center">
-                        <button class="edit_btn" type="submit">Login</button>
-                        <Link to={'/Forgot'}>Forgot Password ?</Link>
-                        <Link to={'/'}class="edit_btn" type="submit">Close</Link>
-                    </div>
-                </form>
+                {
 
+                    isloading ? <div class="text-center">
+                        <div class="spinner-border" role="status" style={{ width: "3rem", height: "3rem", color: '#1eab9b', marginTop: "50%" }}>
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </div> :
+                        <form className='col-lg-3 container auth ' onSubmit={formik.handleSubmit}>
+                            <div className='text-center mb-5'>
+                                <img src='https://wishlistmember.com/wp-content/uploads/2021/08/fluentCRM.png' style={{ width: "50px", borderRadius: "5px" }}></img>
+                                <h4>fluentCRM</h4>
+                            </div>
+                            <input name='Email' placeholder='Email' className='form-control mb-5' onChange={formik.handleChange} value={formik.values.Email}></input>
+                            <input name='Password' type="password" placeholder='Password' className='form-control mb-5' onChange={formik.handleChange} value={formik.values.Password}></input >
+                            <div class="d-grid gap-2 mt-2 text-center">
+                                <button class="edit_btn" type="submit">Login</button>
+                                <Link to={'/Forgot'}>Forgot Password ?</Link>
+                                <Link to={'/'} class="edit_btn" type="submit">Close</Link>
+                            </div>
+                        </form>
+                }
             </div>
 
         </div>
